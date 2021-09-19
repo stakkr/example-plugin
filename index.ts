bdb939ef89d0359f5e46eed01feb11413ffc3891
Luke Smetham <luke@arkitek.co.uk>
2021-09-19 13:26:27 +0200

    wip

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Payload } from './types.ts';
+
 /**
  * Delay util.
  * @param ms number
@@ -9,22 +11,7 @@ function delay(ms: number = 500): Promise<void> {
 	})
 }
 
-/**
- * Takes the Stakkr payload and returns a mutated version of it.
- * @param payload
- * @returns TestPluginPayload
- */
-
-type InputPayload = {
-	[key: string]: any;
-}
-
-type TestPluginPayload = {
-	[key: string]: any;
-	itWorked: boolean;
-};
-
-export async function testPlugin(payload: InputPayload): Promise<TestPluginPayload> {
+export async function testPlugin(payload: Payload): Promise<Payload> {
 	await delay(1000); // simulate 1s of network activity.
 	return {
 		...payload,
---
+++ types.ts
@@ -0,0 +1,4 @@
+//? I think, ideally we have a @stakkr/types lib with all the base stuff? - for now, storing it here with the example plugins.
+export type Payload = {
+	[key: string]: any;
+};
