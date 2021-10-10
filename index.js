5cd9f1f3eb17846ac4a00e7c90be4deed68a0807
Luke Smetham <luke@arkitek.co.uk>
2021-10-10 18:51:51 +0200

    wip

--- index.js
+++ index.js
@@ -4,22 +4,10 @@
  * @returns payload
  */
 async function testPlugin(payload) {
-	await delay(1000); // simulate 1s of network activity.
 	return {
 		...payload,
 		itWorked: true,
 	};	
-}
-
-/**
- * Delay util.
- * @param ms number
- * @returns void
- */
-function delay(ms = 500) {
-	return new Promise((res) => {
-		setTimeout(res, ms)
-	})
 }
 
 module.exports = {
