28a196abb08dd9d6579c6b3370be5c0b0099134f
Nick Parsons <nparsons08@gmail.com>
2021-10-12 22:02:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,22 @@
  * @returns payload
  */
 async function testPlugin(payload) {
+	await delay(1000); // simulate 1s of network activity.
 	return {
 		...payload,
 		itWorked: true,
 	};	
+}
+
+/**
+ * Delay util.
+ * @param ms number
+ * @returns void
+ */
+function delay(ms = 500) {
+	return new Promise((res) => {
+		setTimeout(res, ms)
+	})
 }
 
 module.exports = {
