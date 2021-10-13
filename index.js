cad5891fe9954a90d7179d21c6f650adb77c466d
Nick Parsons <nparsons08@gmail.com>
2021-10-12 23:52:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 /**
  * Test plugin implementation for Stakkr
- * @param payload
+ * @param payload 
  * @returns payload
  */
 async function testPlugin(payload) {
-	await delay(1000); // simulate 1s of network activity.
 	return {
 		...payload,
 		itWorked: true,
-	};
-}
-
-/**
- * Delay util.
- * @param ms number
- * @returns void
- */
-function delay(ms = 500) {
-	return new Promise((res) => {
-		setTimeout(res, ms);
-	});
+	};	
 }
 
 module.exports = {
 	testPlugin,
-};
+}
